d8741b60027ab7507c03f1967c8476af239a0801
Ruzbihan <ruzbihanmzm@gmail.com>
2024-10-30 19:58:57 +0530

    joke service

---
+++ src/jokes/jokes.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Joke } from './schemas/joke.schema';
+import { CreateJokeDto } from './dto/create-joke.dto';
+
+@Injectable()
+export class JokesService {
+  constructor(@InjectModel(Joke.name) private readonly jokeModel: Model<Joke>) {}
+
+  async create(createJokeDto: CreateJokeDto): Promise<Joke> {
+    const newJoke = new this.jokeModel(createJokeDto);
+    return newJoke.save();
+  }
+
+  async findAll(): Promise<Joke[]> {
+    return this.jokeModel.find().exec();
+  }
+}
